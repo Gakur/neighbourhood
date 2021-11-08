2c2165eaa3e2bde97ddb040d13b51f70c76260f5
Peter Gakure <petergakure97@gmail.com>
2021-11-08 10:45:16 +0300

    added the edit create post view function

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -50,3 +50,17 @@ def create_neighbourhood(request):
     else:
         form = NeighbourHoodForm()
     return render(request, 'newhood.html', {'form': form})
+
+
+def join_neighbourhood(request, id):
+    neighbourhood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = neighbourhood
+    request.user.profile.save()
+    return redirect('hood')
+
+
+def leave_neighbourhood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = None
+    request.user.profile.save()
+    return redirect('hood')    
